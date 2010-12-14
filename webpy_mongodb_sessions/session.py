67cebf0a3f66147d7e43c63c5aa0562206fcc34b
Joshua Bronson <jabronson@gmail.com>
2010-12-14 18:22:06 -0500

    don't store session dict inside mongodb so opaquely

--- webpy_mongodb_sessions/session.py
+++ webpy_mongodb_sessions/session.py
@@ -1,5 +1,51 @@
-from web.session import Store
+from datetime import datetime
+from pymongo.binary import Binary
+from re import _pattern_type
 from time import time
+from web.session import Store
+
+seq_types = set((tuple, list))
+map_types = set((dict,))
+atomic_types = set((bool, int, long, float, str, unicode, type(None),
+    _pattern_type, datetime))
+
+def needs_encode(obj):
+    '''
+    >>> from re import compile
+    >>> atomics = (True, 1, 1L, 1.0, '', u'', None, compile(''), datetime.now())
+    >>> any(needs_encode(i) for i in atomics)
+    False
+    >>> needs_encode((1, 2, 3))
+    False
+    >>> needs_encode([])
+    False
+    >>> needs_encode([1, [2, 3]])
+    False
+    >>> needs_encode({})
+    False
+    >>> needs_encode({1: {2: 3}})
+    False
+    >>> needs_encode({(1,): [2]})
+    False
+    >>> needs_encode(set())
+    True
+    >>> needs_encode([1, [set()]])
+    True
+    >>> needs_encode({1: {2: set()}})
+    True
+    >>> needs_encode({frozenset(): 1})
+    True
+    '''
+    obtype = type(obj)
+    if obtype in atomic_types:
+        return False
+    if obtype in seq_types:
+        return any(needs_encode(i) for i in obj)
+    if obtype in map_types:
+        return any(needs_encode(k) or needs_encode(v)
+            for (k, v) in obj.iteritems())
+    return True
+
 
 #: field name used for id
 _id = '_id'
@@ -13,6 +59,14 @@ class MongoStore(Store):
         self.collection = db[collection_name]
         self.collection.ensure_index(_atime)
     
+    def encode(self, sessiondict):
+        return dict((k, Binary(Store.encode(self, v)) if needs_encode(v) else v)
+            for (k, v) in sessiondict.iteritems())
+
+    def decode(self, sessiondict):
+        return dict((k, Store.decode(self, v) if type(v) is Binary else v)
+            for (k, v) in sessiondict.iteritems())
+
     def __contains__(self, key):
         return bool(self.collection.find_one({_id: key}))
 
@@ -37,3 +91,7 @@ class MongoStore(Store):
         '''
         cutoff = time() - timeout
         self.collection.remove({_atime: {'$lt': cutoff}})
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
